a1cf7caa6e7473c4cf6f23576e5aec5d6538eeb8
thiago de lima <thiagolimasilva689@gmail.com>
2026-05-06 11:01:17 -0400

    Create tipos de variaveis.py

---
+++ basico/tipos de variaveis.py
@@ -0,0 +1,22 @@
+#varivaveis são uma forma de armazenar ou atribuir algo
+var_int = 12
+#numeros
+var_float = 15.5
+#tipos numeros decimais
+var_string = "Conjunto de caracteres alfanumericos"
+#incluindo simbolos,caracteres
+var_lista = [1,2,"Paulo",'Silva',True]
+#lista de dados/valores
+var_dicionario = {'nome': 'thiago','idade':20}
+#conjunto de dados:valores
+var_tupla = ('Ana','Fernando', 3)
+#lista de dados/valores porém imutável
+var_set = {1999,2000,2001}
+#conjunto de dados numericos
+var_bool = True
+#Booleano/Binario(0 ou 1)
+
+#exibir as variaveis
+print(var_int)
+print(var_float)
+print(var_string)
